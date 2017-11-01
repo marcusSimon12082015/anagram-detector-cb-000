b7b0c36f7e6aa86c2dc5f0d0c5f9b96cb66f960b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-01 16:09:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,6 +6,11 @@ class Anagram
     @word = word
   end
   def match(word_array)
-    word_array.map{|word| @word.chars.sort.join == word.chars.sort.join}
+    anagrams = []
+    word_array.map{|word|
+      if @word.chars.sort.join == word.chars.sort.join
+        anagrams << word
+      end
+      }
   end
 end
